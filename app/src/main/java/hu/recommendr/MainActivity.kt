aa4d9ae19b307656378a7548cadb6d123641bf3c
Tick Anna <75945220+anna806@users.noreply.github.com>
2024-05-01 15:54:46 +0200

    Creating main page

--- app/src/main/java/hu/recommendr/MainActivity.kt
+++ app/src/main/java/hu/recommendr/MainActivity.kt
@@ -1,15 +1,37 @@
 package hu.recommendr
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import hu.recommendr.ui.theme.RecommendRTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +44,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    MainPage()
                 }
             }
         }
@@ -30,17 +52,32 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MainPage() {
+    Column(modifier = Modifier
+        .fillMaxSize()
+        .background(Color(0xFF536891)), horizontalAlignment = Alignment.CenterHorizontally) {
+        Text(
+            AnnotatedString("RecommendR"),
+            fontSize = 40.sp,
+            color = Color.White,
+            modifier = Modifier.padding(top = 50.dp, bottom = 16.dp))
+        Button(
+            onClick = { /*TODO*/ },
+            colors = ButtonDefaults.buttonColors(containerColor = Color(0xFFf2dd6f))) {
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Icon(painter = painterResource(id = R.drawable.baseline_autorenew_24), contentDescription = "Generate")
+                Text("Generate", fontSize = 20.sp, color = Color.Black, modifier = Modifier.padding(8.dp))
+            }
+        }
+
+    }
+
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun DefaultPreview() {
     RecommendRTheme {
-        Greeting("Android")
+        MainPage()
     }
 }
